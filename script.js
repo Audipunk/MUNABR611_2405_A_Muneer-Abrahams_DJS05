d32ea8eafc1a861771fc4b24a2e5e6a249cf660a
Muneer Abrahams <audipunkwp@gmail.com>
2025-02-12 18:00:01 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 import { createStore } from './store.js';
 import { tallyReducer } from './reducers.js';
-import { add, subtract, reset } from './actions.js';
+import { add, subtract, reset } from './functions.js';
